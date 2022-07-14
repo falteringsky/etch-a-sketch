0f403f1a54c544b8a6cbb3df71ed74e11fc842af
Reinaldo <sora2854@gmail.com>
2022-07-13 22:42:17 -0400

    added click for drawing and footer

--- main.js
+++ main.js
@@ -57,3 +57,7 @@ function resetBoard() {
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => (div.style.backgroundColor = "white"));
   }
+
+  document.querySelector('body').addEventListener('click', () => {
+    click = !click;
+  })
